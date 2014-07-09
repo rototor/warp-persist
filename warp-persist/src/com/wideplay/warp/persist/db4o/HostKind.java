7e9a974aa92e902ffc0db6a8552491709bd81a80
Emmeran Seehuber <rototor@rototor.de>
2014-07-09 12:17:56 +0200

    Started to drop everything which is not warp-persist hibernate related. If somebody wants to use JPA -> Just use guice-persist.

--- warp-persist/src/com/wideplay/warp/persist/db4o/HostKind.java
+++
@@ -1,8 +0,0 @@
-package com.wideplay.warp.persist.db4o;
-
-/**
- * @author Robbie Vanbrabant
- */
-enum HostKind {
-    REMOTE, LOCAL, FILE
-}
--- warp-persist/src/com/wideplay/warp/persist/db4o/package-info.java
+++
@@ -1,4 +0,0 @@
-/**
- * Warp Persist's DB4O support.
- */
-package com.wideplay.warp.persist.db4o;
